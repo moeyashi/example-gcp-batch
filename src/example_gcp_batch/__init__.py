47ea48512a46b2ad41f83769e70588da43582c58
Ren Adachi <49895682+moeyashi@users.noreply.github.com>
2023-08-23 10:17:33 +0900

    feat: add filter with multiple conditions

--- src/example_gcp_batch/__init__.py
+++ src/example_gcp_batch/__init__.py
@@ -11,3 +11,15 @@ def list_filtered_jobs(parent: str):
     request = batch_v1.ListJobsRequest(parent=parent, filter=f'status.state:FAILED')
     page_result = client.list_jobs(request=request)
     return page_result
+
+def list_finished_jobs(parent: str):
+    client = batch_v1.BatchServiceClient()
+    # NG filter examples
+    # status.state:SUCCEEDED|FAILED # invalid list filter: Invalid literal \'SUCCEEDED|FAILED\' for \'resource.status.state\'.
+    # status.state ~ SUCCEEDED|FAILED # invalid list filter: Invalid filter syntax: syntax error at line 1, column 13, token \'~\'
+    # status.state = "(SUCCEEDED,FAILED)" # invalid list filter: Invalid literal \'(SUCCEEDED,FAILED)\' for \'resource.status.state\'
+    # status.state = ("SUCCEEDED","FAILED") # invalid list filter: Invalid filter syntax: syntax error at line 1, column 27, token \',\'
+    # status.state = ("SUCCEEDED"\,"FAILED") # invalid list filter: Invalid filter syntax: syntax error at line 1, column 16, token \'\"SUCCEEDED\"\'
+    request = batch_v1.ListJobsRequest(parent=parent, filter='status.state = "SUCCEEDED" OR status.state = "FAILED"')
+    page_result = client.list_jobs(request=request)
+    return page_result
